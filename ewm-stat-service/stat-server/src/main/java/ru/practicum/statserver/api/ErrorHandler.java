1b4b2aec95fef674e65d60c976ac5190390a0a59
Leonid <walya7534@yandex.ru>
2023-10-12 21:12:50 +0700

    feat: Error Handler for Stat Service

---
+++ ewm-stat-service/stat-server/src/main/java/ru/practicum/statserver/api/ErrorHandler.java
@@ -0,0 +1,20 @@
+package ru.practicum.statserver.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequestException(final BadRequestException e) {
+        return Map.of(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+    }
+}
